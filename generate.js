09292452fa085c9811dc646591662dcd1e6d6856
~nodreb-borrus <nodreb@borr.us>
2023-04-04 13:14:40 -0600

    Generate image even if it already exists, colorway -> colors

--- generate.js
+++ generate.js
@@ -5,23 +5,22 @@ import fs from 'fs'
 function generateSigil(folder, name) {
   const n = name.replace('~','');
   const svg_file = './' + folder + '/' + n + '.svg';
-  if (!fs.existsSync(svg_file)) {
-    const r = sigil({
-      patp: n,
-      renderer: stringRenderer,
-      size: 550,
-      colorway: ["#232323","#ffffff"]
-    });
 
-    if (!fs.existsSync(folder)){
-      fs.mkdirSync(folder, { recursive: true });
-    }
+  const r = sigil({
+    patp: n,
+    renderer: stringRenderer,
+    size: 550,
+    colors: ["#aaaaaa","#ffffff"]
+  });
 
-    fs.writeFile(svg_file, r, function(err, data) {
-      if (err) console.log(err);
-      console.log("Successfully Written to File.");
-    });
+  if (!fs.existsSync(folder)){
+    fs.mkdirSync(folder, { recursive: true });
   }
+
+  fs.writeFile(svg_file, r, function(err, data) {
+    if (err) console.log(err);
+    console.log("Successfully Written to File.");
+  });
 }
 
 
