c6e464286942f2dde41a745e3a44a0d2e836208f
~nodreb-borrus <nodreb@borr.us>
2023-04-04 13:03:06 -0600

    Sigil rendering example

---
+++ README.md
@@ -0,0 +1,6 @@
+An example of how to render an urbit sigil to an svg file.
+
+```
+npm install
+npm run gen
+```
---
+++ generate.js
@@ -0,0 +1,28 @@
+import { sigil, stringRenderer } from '@tlon/sigil-js'
+import fs from 'fs'
+
+
+function generateSigil(folder, name) {
+  const n = name.replace('~','');
+  const svg_file = './' + folder + '/' + n + '.svg';
+  if (!fs.existsSync(svg_file)) {
+    const r = sigil({
+      patp: n,
+      renderer: stringRenderer,
+      size: 550,
+      colorway: ["#232323","#ffffff"]
+    });
+
+    if (!fs.existsSync(folder)){
+      fs.mkdirSync(folder, { recursive: true });
+    }
+
+    fs.writeFile(svg_file, r, function(err, data) {
+      if (err) console.log(err);
+      console.log("Successfully Written to File.");
+    });
+  }
+}
+
+
+generateSigil(".", "nodreb-borrus")
